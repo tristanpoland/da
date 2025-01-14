09f038f3081369c3f7618e9d03450b3673ccb25a
cospplredman <cospplredman@gmail.com>
2025-01-14 13:26:02 -0600

    performance boost?

--- qtree.js
+++ qtree.js
@@ -26,99 +26,141 @@ push: (val) => obj.set(end++, val),
 pop: (val) => obj.get(--end),
 get_end: () => end,
 set_end: (ind) => end = ind,
-clear: () => {
-	end = 0;
-}
+clear: () => end = 0,
 	}
 
 	return obj;
 }
 
-let LinearQtree = (buffer = DynUint8()) => {
-	let col_memo = new Map(); //??
-	let memo = new Map();
+
+let Uint32Arr = (u8buf) => {
+	let obj = {
+get_buffer: () => u8buf,
+set: (ind, num) => {
+	ind *= 4;
+	u8buf.set(ind++, (num >> 0) & 0xff);
+	u8buf.set(ind++, (num >> 8) & 0xff);
+	u8buf.set(ind++, (num >> 16) & 0xff);
+	u8buf.set(ind++, (num >> 24) & 0xff);
+},
+push: (num) => {
+	u8buf.push((num >> 0) & 0xff);
+	u8buf.push((num >> 8) & 0xff);
+	u8buf.push((num >> 16) & 0xff);
+	u8buf.push((num >> 24) & 0xff);
+},
+get: (ind) => {
+	let num = 0;
+	ind *= 4;
+	num += u8buf.get(ind++) << 0;
+	num += u8buf.get(ind++) << 8;
+	num += u8buf.get(ind++) << 16;
+	num += u8buf.get(ind++) << 24;
+	return num;
+},
+pop: () => {
+	let ret = obj.get(obj.get_end()-1)
+	obj.set_end(obj.get_end()-1);
+	return ret;
+},
+get_end: () => u8buf.get_end() / 4,
+set_end: (ind) => u8buf.set_end(ind * 4),
+clear: () => u8buf.clear()
+	};
+
+	return obj;
+}
+
+
+let LinearQtree = (buffer = Uint32Arr(DynUint8())) => {
+	let ab, col_ab, memo, col_memo;
+	//let col_memo = new Map(); //??
+	//let memo = new Map();
 	//let buffer = new Uint8Array(256 * 4);
 
 	let obj = {
 node_at: (ind) => {
 	let node = {
 		ind,
-		get_node: (i) => obj.node_at(obj.read_uint(ind * 5 + 1 + i)),
-		get_tag: () => obj.read_uint(ind * 5),
-		set_tag: (t) => obj.write_uint(ind * 5, t),
-		set_node: (i, nd) => obj.write_uint(ind * 5 + i + 1, nd.ind),
+		get_node: (i) => obj.node_at(buffer.get(ind * 5 + 1 + i)),
+		get_tag: () => buffer.get(ind * 5),
+		set_tag: (t) => buffer.set(ind * 5, t),
+		set_node: (i, nd) => buffer.set(ind * 5 + i + 1, nd.ind),
 	};
 
 	return node;
-},
-
-write_uint: (ind, num) => {
-	ind *= 4;
-	buffer.set(ind++, (num >> 0) & 0xff);
-	buffer.set(ind++, (num >> 8) & 0xff);
-	buffer.set(ind++, (num >> 16) & 0xff);
-	buffer.set(ind++, (num >> 24) & 0xff);
-},
-push_uint: (num) => {
-	buffer.push((num >> 0) & 0xff);
-	buffer.push((num >> 8) & 0xff);
-	buffer.push((num >> 16) & 0xff);
-	buffer.push((num >> 24) & 0xff);
-},
-read_uint: (ind) => {
-	let num = 0;
-	ind *= 4;
-	num += buffer.get(ind++) << 0;
-	num += buffer.get(ind++) << 8;
-	num += buffer.get(ind++) << 16;
-	num += buffer.get(ind++) << 24;
-	return num;
 },
 
 add_node: (t, ...nodes) => {
 	let vals = [t, ...nodes.map(v => v?.ind)];
-	let my_ind = buffer.get_end()/20;
+	let my_ind = buffer.get_end()/5;
 
 	while(vals.length != 5)
 		vals.push(my_ind);
 
-	vals.map(obj.push_uint);
+	vals.map(buffer.push);
 	return obj.node_at(my_ind);
 },
 
 color: (t) => {
-	if(col_memo.has(t))
-		return obj.node_at(col_memo.get(t));
-
 	let node = obj.add_node(t);
-	col_memo.set(t, node.ind);
 
-	let vals = Array(4).fill(buffer.get_end() / 20);
-	let hash = vals.reduce((a, b) => (a << 32n) + BigInt(b), 0n);
-	memo.set(hash, node.ind);
+	col_ab.insert(col_memo, node.ind, (ind) => {
+		buffer.set_end(buffer.get_end() - 5);
+		node = obj.node_at(ind);
+	});
+
+	ab.insert(memo, node.ind, (ind) => {
+		buffer.set_end(buffer.get_end() - 5);
+		node = obj.node_at(ind);
+	});
+
 	return node;
 },
-
 node: (...nodes) => {
 	let t = avg_color(nodes.map(v => v.get_tag()));
-	let vals = nodes.map(v => v.ind);
-	let hash = vals.reduce((a, b) => (a << 32n) + BigInt(b), 0n);
-	if(memo.has(hash))
-		return obj.node_at(memo.get(hash));
-
 	let node = obj.add_node(t, ...nodes);
-	memo.set(hash, node.ind);
-	return node;
 
+	ab.insert(memo, node.ind, (ind) => {
+		buffer.set_end(buffer.get_end() - 5);
+		node = obj.node_at(ind)
+	});
+
+	return node;
 },
 
 get_buffer: () => buffer,
-get_memo: () => memo,
-get_size: () => buffer.get_end()/20
+get_memo: () => [ab, col_ab, memo, col_memo],
+get_size: () => buffer.get_end()/5
 	};
+
+
+	ab = _24_tree(Uint32Arr(DynUint8()), 
+		(a, b) => hash_node(obj.node_at(a)) < hash_node(obj.node_at(b)),
+		(a, b) => hash_node(obj.node_at(a)) == hash_node(obj.node_at(b))
+	);
+
+	col_ab = _24_tree(Uint32Arr(DynUint8()), 
+		(a, b) => obj.node_at(a).get_tag() < obj.node_at(b).get_tag(),
+		(a, b) => obj.node_at(a).get_tag() == obj.node_at(b).get_tag()
+	);
+
+	memo = ab.node_at(0);
+	col_memo = col_ab.node_at(0);
+
 
 	return obj;
 }
+
+
+
+
+
+
+
+
+
+
 
 let log = (fn) => (...x) => (console.log(...x), fn(...x));
 let id = (x) => x;
@@ -350,71 +392,63 @@ get_tree: () => qtree
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 let index_heap = (buffer, is_lt) => {
 	let obj = {
-write_uint: (ind, num) => {
-	ind *= 4;
-	buffer.set(ind++, (num >> 0) & 0xff);
-	buffer.set(ind++, (num >> 8) & 0xff);
-	buffer.set(ind++, (num >> 16) & 0xff);
-	buffer.set(ind++, (num >> 24) & 0xff);
-},
-push_uint: (num) => {
-	buffer.push((num >> 0) & 0xff);
-	buffer.push((num >> 8) & 0xff);
-	buffer.push((num >> 16) & 0xff);
-	buffer.push((num >> 24) & 0xff);
-},
-pop_uint: () => {
-	let num = 0;
-	num += buffer.pop() << 24;
-	num += buffer.pop() << 16;
-	num += buffer.pop() << 8;
-	num += buffer.pop() << 0;
-	return num;
-},
-read_uint: (ind) => {
-	let num = 0;
-	ind *= 4;
-	num += buffer.get(ind++) << 0;
-	num += buffer.get(ind++) << 8;
-	num += buffer.get(ind++) << 16;
-	num += buffer.get(ind++) << 24;
-	return num;
-},
 get_buffer: () => buffer,
 insert: (ind) => {
-	let buf_pos = buffer.get_end() / 4;
-	obj.push_uint(ind);
+	let buf_pos = buffer.get_end();
+	buffer.push(ind);
 	obj.sift_down(buf_pos);
 },
 extract_min: () => {
-	let ret = obj.read_uint(0);
-	obj.write_uint(0, obj.pop_uint());
-	obj.sift_up(0)
+	let ret = buffer.get(0);
+	buffer.set(0, buffer.pop());
+	obj.sift_up(0);
 	return ret;
 },
 cas: (a, b) => {
-	let a_val = obj.read_uint(a);
-	let b_val = obj.read_uint(b);
+	let a_val = buffer.get(a);
+	let b_val = buffer.get(b);
 
 	if(is_lt(a_val, b_val))
 		return false;
 
-	obj.write_uint(b, a_val);
-	obj.write_uint(a, b_val);
+	buffer.set(b, a_val);
+	buffer.set(a, b_val);
 	return true;
 },
 sift_up: (ind) => {
 	let l_ind = (ind + 1) * 2 - 1
 	let r_ind = l_ind + 1;
 
-	if(l_ind >= buffer.get_end() / 4)
+	if(l_ind >= buffer.get_end())
 		return;
 
 	let take = l_ind;
-	if(r_ind < buffer.get_end() / 4){
-		if(is_lt(obj.read_uint(r_ind), obj.read_uint(l_ind)))
+	if(r_ind < buffer.get_end()){
+		if(is_lt(buffer.get(r_ind), buffer.get(l_ind)))
 			take = r_ind;
 	}
 
@@ -465,72 +499,117 @@ free: (ind) => heap.insert(ind)
 
 
 let _24_tree = (buffer, is_lt, is_eq) => {
-	let arena = Arena(buffer, 8 * 4);
-	let root;
+	let arena = Arena(buffer.get_buffer(), 8 * 4);
 
 	let obj = {
-write_uint: (ind, num) => {
-	ind *= 4;
-	buffer.set(ind++, (num >> 0) & 0xff);
-	buffer.set(ind++, (num >> 8) & 0xff);
-	buffer.set(ind++, (num >> 16) & 0xff);
-	buffer.set(ind++, (num >> 24) & 0xff);
-},
-push_uint: (num) => {
-	buffer.push((num >> 0) & 0xff);
-	buffer.push((num >> 8) & 0xff);
-	buffer.push((num >> 16) & 0xff);
-	buffer.push((num >> 24) & 0xff);
-},
-read_uint: (ind) => {
-	let num = 0;
-	ind *= 4;
-	num += buffer.get(ind++) << 0;
-	num += buffer.get(ind++) << 8;
-	num += buffer.get(ind++) << 16;
-	num += buffer.get(ind++) << 24;
-	return num;
-},
 node_at: (ind) => {
 	let node = {
 		ind,
-		size: () => obj.read_uint(ind),
-		set_size: (val) => obj.write_uint(ind, val),
-		get_key: (i) => obj.read_uint(ind + 1 + i),
-		set_key: (i, key) => obj.write_uint(ind + 1 + i, key),
-		get_node: (i) => obj.read_uint(ind + 4 + i),
-		set_node: (i, node) => obj.write_uint(ind + 1 + i, node.ind),
+		size: () => buffer.get(ind),
+		get_size: () => buffer.get(ind),
+		set_size: (val) => buffer.set(ind, val),
+		get_key: (i) => buffer.get(ind + 1 + i),
+		set_key: (i, key) => buffer.set(ind + 1 + i, key),
+		get_node: (i) => obj.node_at(buffer.get(ind + 4 + i)),
+		set_node: (i, node) => buffer.set(ind + 4 + i, node.ind),
 	};
 
 	return node;
 },
-add_node: (size) => {
-	let node = node_at(arena.alloc());
-	node.set_size(size);
+add_node: (keys, nodes) => {
+	let node = obj.node_at(arena.alloc());
+	node.set_size(Math.max(keys.length + 1, nodes.length));
+	keys.forEach((v, i) => node.set_key(i, v));
+	nodes.forEach((v, i) => node.set_node(i, v));
 	return node;
 },
 search: (node, key) => {
 	if(node.ind == 0)
 		return null;
 
-	let cur_key;
-	for(let i = 0; i < root.get_size() - 1; i++){
-		cur_key = node.get_key(i);
-		if(is_lt(key, cur_key)){
-			if(is_eq(key, cur_key))
-				return cur_key;
+	let i = 0;
+	for(; i < node.get_size() - 1; i++){
+		let cur_key = node.get_key(i);
+		if(is_eq(key, cur_key))
+			return cur_key;
+
+		if(is_lt(key, cur_key))
 			break;
-		}
 	}
 
-	let next_node = cur_key;
-	if(is_lt(cur_key, key))
-		next_node = cur_key + 1;
-
-	return obj.search(node.get_node(next_node), key);
+	return obj.search(node.get_node(i), key);
 },
-insert: (node, key) => {
+node_split: (keys, nodes, ind) => {
+	let l_keys = keys.slice(0, ind);
+	let m_key = keys[ind];
+	let r_keys = keys.slice(ind+1);
 
+	let l_nodes = nodes.slice(0, ind+1);
+	let r_nodes = nodes.slice(ind+1);
+
+	return [
+		m_key,
+		obj.add_node(l_keys, l_nodes), 
+		obj.add_node(r_keys, r_nodes)
+	];
+},
+node_insert: (node, ind, key, repl) => {
+	let keys = Array(node.get_size() - 1).fill().map((v, i) => node.get_key(i));
+	let nodes = Array(node.get_size()).fill().map((v, i) => node.get_node(i));
+
+	let new_keys = [...keys.slice(0, ind), key, ...keys.slice(ind)];
+	let new_nodes = [...nodes.slice(0, ind), ...repl, ...nodes.slice(ind + 1)];
+
+	if(new_keys.length > 3)
+		return obj.node_split(new_keys, new_nodes, 2);
+
+	new_keys.forEach((v, i) => node.set_key(i, v)); //kinda gross, but constant cost...
+	new_nodes.forEach((v, i) => node.set_node(i, v));
+	node.set_size(new_nodes.length);	
+
+	return [node];
+},
+insert_: (node, key, fn) => {
+	if(node.ind == 0)
+		return [key, obj.node_at(0), obj.node_at(0)];
+
+	let cur_key;
+	let i = 0;
+	for(; i < node.get_size() - 1; i++){
+		cur_key = node.get_key(i);
+		if(is_eq(key, cur_key)){
+			fn(cur_key);
+			return [node];
+		}
+
+		if(is_lt(key, cur_key))
+			break;
+	}
+
+	let next_node = i;
+
+	let next = obj.insert_(node.get_node(next_node), key, fn);
+	if(next.length == 1){
+		node.set_node(next_node, next[0]);
+		return [node];
+	}
+
+	return obj.node_insert(node, next_node, next[0], [next[1], next[2]]);
+},
+insert: (node, key, fn = () => null) => {
+	let next = obj.insert_(node, key, fn);
+	if(next.length == 1)
+		return next[0];
+	return obj.add_node([next[0]], [next[1], next[2]]);
+},
+to_json: (node) => {
+	if(node.ind == 0)
+		return {};
+
+	let keys = Array(node.get_size() - 1).fill().map((v, i) => node.get_key(i));
+	let nodes = Array(node.get_size()).fill().map((v, i) => node.get_node(i)).map(obj.to_json);
+
+	return {keys, nodes};
 }
 	};
 
